febaabc5a34540ea123023070a2edaa5b7dcaf74
Gabriel Femi <gabrielfemi799@gmail.com>
2020-10-18 12:11:32 +0100

    Start new payment service - Paymint.

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PaymentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,4 +18,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['prefix' => 'v1'], static function () {
+    Route::get('/', function () {
+        return response()->json(['message' => 'Welcome to Paymint']);
+    });
+
+    Route::post('/', [PaymentController::class, 'debit']);
+});
+
 
